64c856960f5bdef3ec24986dc68bbd1cab299288
Mihai Ionescu <intellidream@live.com>
2026-01-01 20:12:20 +0200

    MacOS Google Auth fix

--- Platforms/MacCatalyst/AppDelegate.cs
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -7,5 +7,28 @@ namespace Daily
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 
+        [Export("application:openURL:options:")]
+        public override bool OpenUrl(UIKit.UIApplication app, Foundation.NSUrl url, Foundation.NSDictionary options)
+        {
+            if (url != null)
+            {
+                 // Handle Google Auth Callback
+                 if (url.Scheme == "com.intellidream.daily" && (url.Host == "login-callback" || url.Path == "login-callback"))
+                 {
+                     // Parse code
+                     string urlStr = url.AbsoluteString;
+                     var components = new Foundation.NSUrlComponents(url, true);
+                     var code = components.QueryItems?.FirstOrDefault(q => q.Name == "code")?.Value;
+                     
+                     if (!string.IsNullOrEmpty(code) && Daily.Services.AuthService.GoogleAuthTcs != null && !Daily.Services.AuthService.GoogleAuthTcs.Task.IsCompleted)
+                     {
+                         Daily.Services.AuthService.GoogleAuthTcs.TrySetResult(code);
+                         return true;
+                     }
+                 }
+            }
+            return base.OpenUrl(app, url, options);
+        }
+
     }
 }
