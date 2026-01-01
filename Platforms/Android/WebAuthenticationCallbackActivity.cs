5d307c16be8878d2403c44a8bc3391f26db6feb3
Mihai Ionescu <intellidream@live.com>
2026-01-01 07:40:25 +0200

    Another attempt

--- Platforms/Android/WebAuthenticationCallbackActivity.cs
+++ Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -4,9 +4,6 @@ using Android.Content.PM;
 namespace Daily.Platforms.Android
 {
     [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true, Name = "com.intellidream.daily.WebAuthenticationCallbackActivity")]
-    [IntentFilter(new[] { global::Android.Content.Intent.ActionView },
-                  Categories = new[] { global::Android.Content.Intent.CategoryDefault, global::Android.Content.Intent.CategoryBrowsable },
-                  DataScheme = "com.intellidream.daily")]
     public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
     {
     }
