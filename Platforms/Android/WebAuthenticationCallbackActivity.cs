f4bfc5c830b48d79ae12469169e2c113ce35fbe9
Mihai Ionescu <intellidream@live.com>
2026-01-01 05:19:12 +0200

    Attempt 7

--- Platforms/Android/WebAuthenticationCallbackActivity.cs
+++ Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -6,8 +6,7 @@ namespace Daily.Platforms.Android
     [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
     [IntentFilter(new[] { global::Android.Content.Intent.ActionView },
                   Categories = new[] { global::Android.Content.Intent.CategoryDefault, global::Android.Content.Intent.CategoryBrowsable },
-                  DataScheme = "com.intellidream.daily",
-                  DataHost = "login-callback")]
+                  DataScheme = "com.intellidream.daily")]
     public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
     {
     }
