f4d58af091441cfcdb0219bcb9907e3d638f8496
Mihai Ionescu <intellidream@live.com>
2026-01-01 07:33:00 +0200

    Another Android Google Login fix attempt

--- Platforms/Android/WebAuthenticationCallbackActivity.cs
+++ Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -3,11 +3,10 @@ using Android.Content.PM;
 
 namespace Daily.Platforms.Android
 {
-    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true, Name = "com.intellidream.daily.WebAuthenticationCallbackActivity")]
     [IntentFilter(new[] { global::Android.Content.Intent.ActionView },
                   Categories = new[] { global::Android.Content.Intent.CategoryDefault, global::Android.Content.Intent.CategoryBrowsable },
-                  DataScheme = "com.intellidream.daily",
-                  DataHost = "login-callback")]
+                  DataScheme = "com.intellidream.daily")]
     public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
     {
     }
