b20c97631ef6375f177fa45c4d1aa93fa94ba737
Mihai Ionescu <intellidream@live.com>
2026-04-15 19:28:55 +0300

    Windows SysTray support fixes to also let Mac/iOS/Android work

--- Platforms/Windows/WindowsTrayService.cs
+++ Platforms/Windows/WindowsTrayService.cs
@@ -1,16 +1,40 @@
 using Daily.Services;
 using System;
 using Microsoft.Maui.Controls;
+using H.NotifyIcon;
 
 namespace Daily.Platforms.Windows
 {
     public class WindowsTrayService : ITrayService
     {
         public Action? ClickHandler { get; set; }
+        private TaskbarIcon? _trayIcon;
 
         public void Initialize() 
         {
-            // Initialization is handled in XAML (App.xaml) and code behind.
+            if (_trayIcon == null)
+            {
+                var menu = new MenuFlyout();
+                var showItem = new MenuFlyoutItem { Text = "Show" };
+                showItem.Clicked += (s, e) => ClickHandler?.Invoke();
+                
+                var exitItem = new MenuFlyoutItem { Text = "Exit" };
+                exitItem.Clicked += (s, e) => Application.Current?.Quit();
+                
+                menu.Add(showItem);
+                menu.Add(exitItem);
+
+                _trayIcon = new TaskbarIcon
+                {
+                    IconSource = "appicon_windows.ico",
+                    ToolTipText = "Daily",
+                    ContextFlyout = menu,
+                    LeftClickCommand = new Command(() => ClickHandler?.Invoke())
+                };
+
+                // Create the native tray icon
+                _trayIcon.ForceCreate();
+            }
         }
     }
 }
